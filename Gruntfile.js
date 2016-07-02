257f93e82305819e4d3e504896526941f0134545
Erin Mahoney <emahoneyphoto@gmail.com>
2016-07-02 15:35:46 -0700

    Setting up grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,59 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    fileNamePrefix: 'fluidXPS-tnf',
     sass: {
-      options: {
-        sourceMap: true
-      },
       dist: {
+        options: {
+          style: 'compressed'
+        },
         files: {
-          'dist/assets/css/app.css': 'app/css/style.css'
+          'app/css/style.css': 'app/css/style.scss'
         }
-      }
-    },
-    copy: {
-      images: {
-        files: [{
-          expand: true,
-          cwd: 'app/img',
-          src: ['*.*'],
-          dest: 'dist/assets/img'
-        }]
-      },
-      fonts: {
-        files: [{
-          expand: true,
-          cwd: 'app/fonts',
-          src: ['*.*'],
-          dest: 'dist/assets/fonts'
-        }]
-      },
-      html: {
-        files: [{
-          expand: true,
-          cwd: 'app/pages',
-          src: ['**/*', '!index.html'],
-          dest: 'dist/'
-        }]
-      }
+      } 
     },
     watch: {
-      files: ['app/**'],
-      tasks: ['sass'],
-      options: {
-        spawn: false
-      }
-    },
-    notify_hooks: {
-      options: {
-        enabled: true,
-        max_jshint_notifications: 5,
-        title: 'SearchSolution',
-        success: true
-      }
-    },
-    eslint: {
-      options: {
-        configFile: '.eslintrc'
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
       },
-      target: ['app/**/*.js']
+      scripts: {
+        files: '**/*.js',
+        tasks: ['jshint']
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'app/js/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
     },
     connect: {
       server: {
         options: {
-          base: './dist',
-          port: 8080,
-          open: 'http://localhost:8080'
+          keepalive: true,
+          hostname: 'localhost',
+          port: '8080',
+          base: 'app/pages',
+          livereload: true
         }
       }
     }
   });
-
-  // Load the plugin that provides the task.
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-notify');
-  grunt.loadNpmTasks('grunt-eslint');
 
-  grunt.task.run('notify_hooks');
+  grunt.registerTask('default', ['watch', 'connect']);
 
-  // Default task(s).
-  grunt.registerTask('default', [
-    'eslint',
-    'sass',
-    'sass',
-    'connect:server',
-    'watch:local'
-  ]);
 };
